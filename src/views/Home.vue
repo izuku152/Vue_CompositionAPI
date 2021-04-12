f060542cfc30735d2b9712224515cc877639e0ee
Izuku152e <hassanhanif123429@gmail.com>
2021-04-12 21:01:08 +0500

    Template Refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,44 +2,38 @@
   <div class="home">
     <h1>Composition API Basics</h1>
     <br />
-    <h3>Setup</h3>
+    <h3 ref="p">Setup</h3>
     <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click Me</button>
-    <h3>Options</h3>
-    <p>My name is {{ namee }} and my age is {{ agee }}</p>
-    <button @click="handleClicke">Click Me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   // Composition API runs before options API
   setup() {
     let name = "Hassan";
-    let age = 50;
+    let age = 20;
+
+    // to use template ref like in Options API
+    // We need to import it
+    // We hardly use template refs
+    // We can use it in another way
+
+    let p = ref(null);
+    console.log(p); // Ref will be Null
+    console.log(p.value); // Ref Value will be Null
 
     const handleClick = () => {
-      console.log("You Clicked Me");
+      console.log(p); // Ref will point h3 in template
+      console.log(p.value); //Ref Value will be h3
+      p.value.textContent = "Changed Text Content";
     };
 
     // We must always return whatever we want to use inside of template
-    return { name, age, handleClick };
-  },
-
-  // We can use both Options and Composition API
-  // they cannot communicate b/w each other
-  // Template Refs
-  data() {
-    return {
-      namee: "M. Hassan Hanif Khan",
-      agee: 20,
-    };
-  },
-  methods: {
-    handleClicke() {
-      console.log("Options handle click");
-    },
+    return { name, age, handleClick, p };
   },
 };
 </script>
