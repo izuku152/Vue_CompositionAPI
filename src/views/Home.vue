02a635c9a75308b1ff7e5d164c453cce16a24351
Izuku152e <hassanhanif123429@gmail.com>
2021-04-12 22:53:27 +0500

    Computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,59 +2,36 @@
   <div class="home">
     <h1>Composition API Basics</h1>
     <br />
-    <h3>Ninja 1</h3>
-    <p>My name is {{ ninja_1.name }} and my age is {{ ninja_1.age }}</p>
-    <button @click="handleClick_1">Update Ninja 1</button>
+    <input type="text" v-model="search" />
     <br />
-    <h3>Ninja 2</h3>
-    <p>My name is {{ ninja_2.name }} and my age is {{ ninja_2.age }}</p>
-    <button @click="handleClick_2">Update Ninja 2</button>
-    <br />
-    <p>{{ name2 }}</p>
-    <button @click="changeName2">Change Name 2</button>
+    <p v-for="name in filteredNames" :key="name">{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { computed, ref } from "vue";
 export default {
   name: "Home",
   setup() {
-    // Refs problem is we have to add .value
-    // Reactive solves this problem but
-    let ninja_1 = ref({ name: "Izuku", age: 20 });
-    let ninja_2 = reactive({ name: "Bakugo", age: 12 });
+    const search = ref("");
+    const names = ref([
+      "mario",
+      "luigi",
+      "shaun",
+      "chun-li",
+      "ninja",
+      "izuku",
+      "bakugo",
+      "todoroki",
+    ]);
+    // We have to import computed first
+    // Computed function accepts a function which should returns a value
+    // How to filter an array based on search value
+    const filteredNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
 
-    const handleClick_1 = () => {
-      ninja_1.value.name = "Izuku Midoriya";
-      ninja_1.value.age = 21;
-    };
-    const handleClick_2 = () => {
-      ninja_2.name = "Katsuki Bakugo";
-      ninja_2.age = 13;
-    };
-    //
-    // Reactive doesn't work with primitive values
-    let name2 = reactive("Mario"); //This is not a reactive value
-    const changeName2 = () => {
-      name2 = "Luigi"; //It will not work
-    };
-
-    // We must always return whatever we want to use inside of template
-    return {
-      ninja_1,
-      ninja_2,
-      handleClick_1,
-      handleClick_2,
-      name2,
-      changeName2,
-    };
-
-    // Takeaways:
-
-    // Refs are much better than reactive
-    // Refs retain their reactivity when exposed to external functions
-    // Use reactive for objects
+    return { search, names, filteredNames };
   },
 };
 </script>
