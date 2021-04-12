f6d73c69d86902493f37f79ea5db5ed660ddddbe
Izuku152e <hassanhanif123429@gmail.com>
2021-04-12 23:22:35 +0500

    Watch & Watch Effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,14 @@
     <br />
     <input type="text" v-model="search" />
     <br />
-    <p v-for="name in filteredNames" :key="name">{{ name }}</p>
+    <p v-for="name in names" :key="name">{{ name }}</p>
+    <br />
+    <button @click="handleClick">Stop Watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 export default {
   name: "Home",
   setup() {
@@ -24,14 +26,26 @@ export default {
       "bakugo",
       "todoroki",
     ]);
-    // We have to import computed first
-    // Computed function accepts a function which should returns a value
-    // How to filter an array based on search value
-    const filteredNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
+    // Watch only runs when something happens to search
+    const stopWatch = watch(search, () => {
+      console.log("Watch funtion Ran");
+    });
+    // Watch Effect runs whenever component loads for the first time or in this case when th setup first loaded
+    // It will start whenever the value of search changes
+    // WatchEffect is mostly used to get data from database
+    // lets say whenever ID changes it feteches data from database
+    const stopEffect = watchEffect(() => {
+      console.log("Watch Effect Ran", search.value);
     });
 
-    return { search, names, filteredNames };
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
+    };
+
+    // What if you want them to stop watching after some condition is true
+    // Just assign thees functions to a variable and call it
+    return { search, names, handleClick };
   },
 };
 </script>
