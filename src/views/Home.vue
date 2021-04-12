dbeb2175aaaa20fe4399b2513025c7fce66adf93
Izuku152e <hassanhanif123429@gmail.com>
2021-04-12 22:17:10 +0500

    Refs vs Reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,34 +2,59 @@
   <div class="home">
     <h1>Composition API Basics</h1>
     <br />
-    <h3 ref="p">Setup</h3>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <h3>Ninja 1</h3>
+    <p>My name is {{ ninja_1.name }} and my age is {{ ninja_1.age }}</p>
+    <button @click="handleClick_1">Update Ninja 1</button>
     <br />
-    <input type="text" v-model="name" />
+    <h3>Ninja 2</h3>
+    <p>My name is {{ ninja_2.name }} and my age is {{ ninja_2.age }}</p>
+    <button @click="handleClick_2">Update Ninja 2</button>
     <br />
-    <button @click="handleClick">Click Me</button>
-    <button @click="age++">Add 1 to Age</button>
+    <p>{{ name2 }}</p>
+    <button @click="changeName2">Change Name 2</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 export default {
   name: "Home",
   setup() {
-    // By default Composition API don't have reactive Values
-    // To make them reactive, we use ref
-    let name = ref("");
-    let age = ref(20);
+    // Refs problem is we have to add .value
+    // Reactive solves this problem but
+    let ninja_1 = ref({ name: "Izuku", age: 20 });
+    let ninja_2 = reactive({ name: "Bakugo", age: 12 });
 
-    const handleClick = () => {
-      // To get value of ref inside setup we add .value
-      // But in template we just write it w/o .value
-      name.value = "Izuku Midoriya";
+    const handleClick_1 = () => {
+      ninja_1.value.name = "Izuku Midoriya";
+      ninja_1.value.age = 21;
+    };
+    const handleClick_2 = () => {
+      ninja_2.name = "Katsuki Bakugo";
+      ninja_2.age = 13;
+    };
+    //
+    // Reactive doesn't work with primitive values
+    let name2 = reactive("Mario"); //This is not a reactive value
+    const changeName2 = () => {
+      name2 = "Luigi"; //It will not work
     };
 
     // We must always return whatever we want to use inside of template
-    return { name, age, handleClick };
+    return {
+      ninja_1,
+      ninja_2,
+      handleClick_1,
+      handleClick_2,
+      name2,
+      changeName2,
+    };
+
+    // Takeaways:
+
+    // Refs are much better than reactive
+    // Refs retain their reactivity when exposed to external functions
+    // Use reactive for objects
   },
 };
 </script>
