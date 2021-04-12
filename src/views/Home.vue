368889840764232322315a48c62a046784d284f5
Izuku152e <hassanhanif123429@gmail.com>
2021-04-12 21:13:48 +0500

    Refs for Reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,11 @@
     <br />
     <h3 ref="p">Setup</h3>
     <p>My name is {{ name }} and my age is {{ age }}</p>
+    <br />
+    <input type="text" v-model="name" />
+    <br />
     <button @click="handleClick">Click Me</button>
+    <button @click="age++">Add 1 to Age</button>
   </div>
 </template>
 
@@ -12,28 +16,20 @@
 import { ref } from "vue";
 export default {
   name: "Home",
-  // Composition API runs before options API
   setup() {
-    let name = "Hassan";
-    let age = 20;
-
-    // to use template ref like in Options API
-    // We need to import it
-    // We hardly use template refs
-    // We can use it in another way
-
-    let p = ref(null);
-    console.log(p); // Ref will be Null
-    console.log(p.value); // Ref Value will be Null
+    // By default Composition API don't have reactive Values
+    // To make them reactive, we use ref
+    let name = ref("");
+    let age = ref(20);
 
     const handleClick = () => {
-      console.log(p); // Ref will point h3 in template
-      console.log(p.value); //Ref Value will be h3
-      p.value.textContent = "Changed Text Content";
+      // To get value of ref inside setup we add .value
+      // But in template we just write it w/o .value
+      name.value = "Izuku Midoriya";
     };
 
     // We must always return whatever we want to use inside of template
-    return { name, age, handleClick, p };
+    return { name, age, handleClick };
   },
 };
 </script>
