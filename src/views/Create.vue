ccd690a2873345ef21f4fd9fbfe9550f19fa5a16
Izuku152e <hassanhanif123429@gmail.com>
2021-04-14 05:53:33 +0500

    Completed + POST Request

---
+++ src/views/Create.vue
@@ -0,0 +1,113 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <label>Title: </label>
+    <input type="text" v-model="title" />
+    <label>Details: </label>
+    <input type="textarea" v-model="body" />
+    <label>Tags: </label>
+    <!-- It will not submit form if we press enter on the tag field -->
+    <input type="text" v-model="tag" @keydown.enter.prevent="handleTag" />
+    <div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
+    <button>Add Post</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const title = ref("");
+    const body = ref("");
+    const tag = ref("");
+    const tags = ref([]);
+
+    const submitForm = async () => {
+      const post = {
+        title: title.value,
+        body: body.value,
+        tags: tags.value,
+      };
+
+      await fetch("http://localhost:3000/projects", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(post),
+      });
+
+      title.value = "";
+      body.value = "";
+      tag.value = "";
+      tags.value = "";
+
+      console.log("Form Submitted");
+    };
+    const handleTag = () => {
+      tag.value = tag.value.replace(/\s/, "");
+      if (!tags.value.includes(tag.value)) {
+        tags.value.push(tag.value);
+      }
+      tag.value = "";
+    };
+
+    return { title, body, tag, handleTag, tags, submitForm };
+  },
+};
+</script>
+
+<style>
+form {
+  max-width: 480px;
+  margin: 0 auto;
+  text-align: left;
+}
+input,
+textarea {
+  display: block;
+  margin: 10px 0;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
+  border: 1px solid #eee;
+}
+textarea {
+  height: 160px;
+}
+label {
+  display: inline-block;
+  margin-top: 30px;
+  position: relative;
+  font-size: 20px;
+  color: white;
+  margin-bottom: 10px;
+}
+label::before {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ff8800;
+  position: absolute;
+  z-index: -1;
+  padding-right: 40px;
+  left: -30px;
+  transform: rotateZ(-1.5deg);
+}
+button {
+  display: block;
+  margin-top: 30px;
+  background: #ff8800;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  font-size: 18px;
+}
+.pill {
+  display: inline-block;
+  margin: 10px 10px 0 0;
+  color: #444;
+  background: #ddd;
+  padding: 8px;
+  border-radius: 20px;
+  font-size: 14px;
+}
+</style>
